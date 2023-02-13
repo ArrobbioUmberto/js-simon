a351dbb2281fe4e46c9237eefd915a86c9f64d26
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-13 20:07:17 +0100

    FIX true also with randow right elements

--- js/app.js
+++ js/app.js
@@ -33,55 +33,26 @@ function myfunction() {
       numberPush.push(userTry);
     }
     console.log(numberPush,Number);
-    if (numberUser === Number) {
-        containerEL.append = `<h1> Complimenti hai azzeccato tutti i numeri. ${Number}</h1>`
-    } else {
-        containerEL.append = `<h1> Mi spiace non hai azzeccato tutti i numeri. Guarda quale hai mancato, ${Number}</h1>`
-    }
-        // console.log(numberPush)
-        // console.log(numberPush, Number)
+    confrontoArray (numberPush,Number)
  }
     
+// FUNZIONI 
 
+// DEVO CREARE UNA FUNZIONE PER METTERE A CONFRONTO I DUE ARRAY CHE SI GENERANO 
 
-// BONUS 
-
-// Pulsante di play che permette di ripetere il gioco ogni volta che clicca il Pulsante
-
-btnEL.addEventListener('click', function(){
-    let Number = []
-    // generare 5 numeri casuali e metterli dentro una costante 
-    while (Number.length < 5){
-            let randomNumber = Math.round(Math.random() * 9) + 1
-            if (!Number.includes(randomNumber)){
-                Number.push(randomNumber)
-            }
-            console.log(randomNumber)
-            console.log(Number)
+function confrontoArray(numberPush, Number) {
+    if (numberPush.length !== Number.length) {      // prima condizione per capire subito se non hanno la stessa lunghezza senno non parte neanche il ciclo for 
+        containerEL.innerHTML = `<h1> Mi spiace non hai azzeccato tutti i numeri. Guarda quale hai mancato, ${Number}</h1>`
+        return false
     }
-    
-    
-    alert(Number)
-    
-    // console.log(numberUser)
-    setTimeout (myfunction,3000)
-    function myfunction() {
-        let numberUser = [];
-        let numberPush = numberUser;
-        
-        while (numberUser.length < 5) {
-          let userTry = parseInt(prompt('Dimmi uno dei numeri che hai appena visto, vediamo se li ricordi tutti'));
-          while (isNaN(userTry) || userTry <= 0) {
-            userTry = parseInt(prompt('Dimmi uno dei numeri che hai appena visto, vediamo se li ricordi tutti'));
-          }
-          numberPush.push(userTry);
+    numberPush.sort()
+    Number.sort()
+    for (let i = 0; i < numberPush.length; i++) {
+        if (numberPush[i] !== Number[i]) {
+            containerEL.innerHTML = `<h1> Mi spiace non hai azzeccato tutti i numeri. Guarda quale hai mancato, ${Number}</h1>`
+            return false
         }
-        console.log(numberPush);
-        if (numberPush = Number){
-            containerEL.append = `<h1> Complimenti hai azzeccato tutti i numeri. ${Number}</h1>` 
-       } else {
-        containerEL.append = `<h1> Mi spiace non hai azzaccato tutti i numeri. Guarda quale hai mancato, ${Number}</h1>`
-       }
-     }
-    
-})
+    }
+    containerEL.innerHTML = `<h1> Complimenti hai azzeccato tutti i numeri. ${Number}</h1>`
+    return true
+}
