35ad90c3a0c2ebfc305dc68cf6619d272c3863e3
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-13 15:43:33 +0100

    FEAT random number without double

--- js/app.js
+++ js/app.js
@@ -6,12 +6,27 @@ console.log(containerEL)
 
 let Number = []
 // generare 5 numeri casuali e metterli dentro una costante 
-for (let i=0; i< 5;i++){
-    let randomNumber = Math.round(Math.random() * 100)
-    Number.push(randomNumber)
-    console.log(randomNumber)
+while (Number.length < 5){
+        let randomNumber = Math.round(Math.random() * 9) + 1
+        if (!Number.includes(randomNumber)){
+            Number.push(randomNumber)
+        }
+        console.log(randomNumber)
+        console.log(Number)
 }
 
-alert(Number)
+
+// alert(Number)
+// let numberUser = ''
+// setTimeout (myfunction,3000)
+// function myfunction(){
+//     for (let i = 0; i < 5;i++){
+//         let userTry = parseInt(prompt('Dimmi uno dei numeri che hai appena visto, vediamo se li ricordi tutti'))
+//         console.log(numberUser,Number)
+//     }
+//     if (Number.includes(numberUser)){
+//         console.log('complimenti hai indovinato tutti i numeri')
+//     }
+// }
 
 // const askNumber = parseInt(prompt('Ti ricordi i numeri che hai appena visto ?'))
